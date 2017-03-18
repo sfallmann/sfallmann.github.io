df8466d0dd9709757f21aae3d1820110c7e00060
Sean Fallmann <jorundr@hotmail.com>
2017-03-18 11:39:30 -0400

    Add export from codepen.io

---
+++ js/index.js
@@ -0,0 +1,55 @@
+//var $titles = $('#header h2');
+//  $titles.slideToggle( 1000, 'swing', function() {
+ //   // Animation complete.
+ // });
+
+var $leftNavLinks = $('.left-nav-item.nav-link');
+var $aboutContent = $('#about .content');
+var $rollup = $('#rollup');
+
+var $firstNavItem = $('.left-nav-item:first-of-type');
+var $titles = $('.titles');
+var $wobble = $('.wobble');
+var $hornsWobble = $('.horns-wobble');
+
+$rollup.on('click', function(){
+  $('#rollup .fa').toggleClass('fa-chevron-down').toggleClass('fa-chevron-up');
+  $leftNavLinks.toggleClass('hidden');
+
+})
+
+function showContent() {
+  $rollup.trigger('click');
+ 
+  var id = $(this).data('anchor');
+  var pos = $(id).position();
+  scrollTo(pos.left, pos.top);
+  var $content = $(id + " .content");
+  $content.hide();
+  $content.fadeIn("slow");
+};
+
+$leftNavLinks.on('click', showContent);
+
+$('.logo').on('click', function(){
+  $('body').scrollTop(0);
+  
+  if (!$titles.hasClass('teleport')){
+    $titles.addClass('teleport');
+  }
+  
+});
+
+$titles.on('webkitAnimationEnd oanimationend msAnimationEnd animationend',   
+  function(e) {
+    $titles.removeClass('teleport');
+  });
+
+$wobble.on('webkitAnimationEnd oanimationend msAnimationEnd animationend',   
+  function(e) {
+    $hornsWobble.removeClass('wobble');
+    setTimeout(function(){
+      $hornsWobble.addClass('wobble');
+    }, 1);
+    //
+  });
